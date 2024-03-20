8d5bce1cfde6f7e89180324f51badb2f971d2d68
Landon Bouma <landonb@users.noreply.github.com>
2024-03-20 18:32:45 -0500

    Linting: Use double quotes around string variables

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -ex
 
-GH_USER='landonb'
-GH_REPO='fries-findup'
-VERSION=0.0.3
+GH_USER="landonb"
+GH_REPO="fries-findup"
+VERSION="0.0.3"
 
-TMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp.XXXXXXXXXX')
+TMPDIR="$(mktemp -d 2>/dev/null || mktemp -d -t "tmp.XXXXXXXXXX")"
 
-REPODIR=${TMPDIR}/${GH_REPO}-${VERSION}
+REPODIR="${TMPDIR}/${GH_REPO}-${VERSION}"
 
 REPOTAR="https://github.com/${GH_USER}/${GH_REPO}/archive/${VERSION}.tar.gz"
 
-cd ${TMPDIR}
-curl -sL ${REPOTAR} | gunzip | tar xf -
-cd ${REPODIR}
+cd "${TMPDIR}"
+curl -sL "${REPOTAR}" | gunzip | tar xf -
+cd "${REPODIR}"
 make install
-cd ${TMPDIR}
+cd "${TMPDIR}"
 command rm -rf -- "${REPODIR}"
 
